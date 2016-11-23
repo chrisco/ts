bd980c40a848edb0d2908d25ca7b25e1cecc706c
Christopher Comella <git.chrisco@gmail.com>
2016-11-23 09:23:14 -0700

    Refactor gulpfile (extract parts to individual files)

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssVars = require('postcss-simple-vars');
+const cssNested = require('postcss-nested');
+const cssImport = require('postcss-import');
+
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssNested, cssVars, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
